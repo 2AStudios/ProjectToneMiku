f73492005b5b714353cebd219901ba89337156c0
2A_Studios <kevinsun08512@gmail.com>
2018-03-14 12:20:48 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        MainWindow application = new MainWindow();
+        MidiPlayer app = new MidiPlayer();
+    }
+}
