36b63b052a63cdaa2d1ae5304cbb730ea7e7a1e0
2A_Studios <kevinsun08512@gmail.com>
2018-03-14 12:45:27 -0400

    Added Midi Deconstruct Method, use later to print out data.

--- src/MidiPlayer.java
+++ src/MidiPlayer.java
@@ -3,12 +3,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Sequencer;
+import javax.sound.midi.*;
 
 public class MidiPlayer {
+    public static final int NOTE_ON = 0x90;
+    public static final int NOTE_OFF = 0x80;
+    public static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-    public MidiPlayer(){
+    public MidiPlayer() {
         try {
             // Obtains the default Sequencer connected to a default device.
             Sequencer sequencer = MidiSystem.getSequencer();
@@ -26,10 +28,54 @@ public class MidiPlayer {
 
             // Starts playback of the MIDI data in the currently loaded sequence.
             sequencer.start();
-        }catch(Exception e){
+
+            MidiReader("audio/ddlctheme.mid");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    public void MidiReader(String file) throws Exception {
+        Sequence sequence = MidiSystem.getSequence(new File(file));
+
+        int trackNumber = 0;
+        for (Track track : sequence.getTracks()) {
+            trackNumber++;
+            System.out.println("Track " + trackNumber + ": size = " + track.size());
+            System.out.println();
+            for (int i = 0; i < track.size(); i++) {
+                MidiEvent event = track.get(i);
+                System.out.print("@" + event.getTick() + " ");
+                MidiMessage message = event.getMessage();
+                if (message instanceof ShortMessage) {
+                    ShortMessage sm = (ShortMessage) message;
+                    System.out.print("Channel: " + sm.getChannel() + " ");
+                    if (sm.getCommand() == NOTE_ON) {
+                        int key = sm.getData1();
+                        int octave = (key / 12) - 1;
+                        int note = key % 12;
+                        String noteName = NOTE_NAMES[note];
+                        int velocity = sm.getData2();
+                        System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+                    } else if (sm.getCommand() == NOTE_OFF) {
+                        int key = sm.getData1();
+                        int octave = (key / 12) - 1;
+                        int note = key % 12;
+                        String noteName = NOTE_NAMES[note];
+                        int velocity = sm.getData2();
+                        System.out.println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+                    } else {
+                        System.out.println("Command:" + sm.getCommand());
+                    }
+                } else {
+                    System.out.println("Other message: " + message.getClass());
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
 }
